5052ee4fd3ed73ce0e6316809fe77f5bc8d772e9
Anastasia <lockeyts@gmail.com>
2022-06-21 20:42:24 +0300

    week25

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Chart } from "react-google-charts";
+
+
+export const data = [
+  ["Artist", "Views (billions)"],
+  ["BTS", 25],
+  ["Blackpink", 14],
+  ["Twice", 9],
+  ["PSY", 7],
+  ["BIGBANG", 4],
+];
+
+export const options = {
+  title: "Most watched k-pop artists",
+};
+
+export function App() {
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"500px"}
+    />
+  );
+}
+
+
+
+export default App;
